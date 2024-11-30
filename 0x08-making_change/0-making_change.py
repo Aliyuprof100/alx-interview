3c70812de38fe05165f0936d9c74809743daebe8
aliyuprof100 <aliyuddeen95@gmail.com>
2024-11-30 07:20:54 +0000

    code changes for the better

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,19 +1,24 @@
 #!/usr/bin/python3
 """ solving making chane problem """
 
-
 def makeChange(coins, total):
-    """ Functions that solve the problem """
-    # If total is 0 or less, return 0 as no coins are needed
+    """Making Changes"""
     if total <= 0:
         return 0
-    # Initialize dp array with a large number, to represent infinity
-    dp = [float('inf')] * (total + 1)
-    dp[0] = 0  # Base case: 0 coins needed to make 0
-    # Iterate over each coin
-    for coin in coins:
-        # Update dp array for amounts from coin to total
-        for amount in range(coin, total + 1):
-            dp[amount] = min(dp[amount], dp[amount - coin] + 1)
-
-    return dp[total] if dp[total] != float('inf') else -1
+    elif total > 0:
+        newList = sorted(coins[:])
+        newList = list(reversed(newList))
+        count = 0
+        value = total + 0
+        index = 0
+        while value >= 0 and (index < len(newList)):
+            if value >= newList[index]:
+                value = value - newList[index]
+                count += 1
+            elif value < newList[index]:
+                index += 1
+        if index == len(newList):
+            if value != 0:
+                return -1
+            elif value == 0:
+                return count
